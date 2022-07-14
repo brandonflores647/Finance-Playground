476953f38a249c0268017b33afd863d920c7ffed
Brandon Flores <brandonflores647@gmail.com>
2022-07-14 18:30:52 -0400

    added reset button functionality and style

--- src/components/CompoundInterestForm/index.js
+++ src/components/CompoundInterestForm/index.js
@@ -1,13 +1,38 @@
+import { useState, useEffect } from 'react';
+
 import './CompoundInterestForm.css'
 
 const CompoundInterestForm = () => {
+
+    const [init, setInit] = useState(1);
+    const [monthlyCont, setMonthlyCont] = useState('');
+    const [time, setTime] = useState(1);
+    const [interest, setInterest] = useState(0);
+    const [freq, setFreq] = useState('Annually');
+
+    const handleReset = () => {
+        setInit(1);
+        setMonthlyCont('');
+        setTime(1);
+        setInterest(0);
+        setFreq('Annually');
+    }
+
     return (
+        <>
+        <section id='compount-top'>
+            <button id='compount-reset-button'
+                    onClick={handleReset}
+                    >RESET</button>
+        </section>
         <form id='compound-interest-form'>
             <label>Initial Investment:</label>
             <input
                 type='number'
                 className='input-field-c'
                 min='1'
+                value={init}
+                onChange={(e) => setInit(e.target.value)}
             />
             <label>Monthly Contribution:</label>
             <input
@@ -15,27 +40,39 @@ const CompoundInterestForm = () => {
                 className='input-field-c'
                 placeholder='(Optional)'
                 min='0'
+                value={monthlyCont}
+                onChange={(e) => setMonthlyCont(e.target.value)}
             />
             <label>Length of time in years:</label>
             <input
                 type='number'
                 className='input-field-c'
                 min='1'
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
             />
             <label>Estimated interest rate:</label>
             <input
                 type='number'
                 className='input-field-c'
                 min='0'
+                value={interest}
+                onChange={(e) => setInterest(e.target.value)}
             />
             <label>
                 Compound Frequency:
-                <select className='input-field-c' id='compount-frequency-input'>
+                <select
+                    className='input-field-c'
+                    id='compount-frequency-input'
+                    value={freq}
+                    onChange={(e) => setFreq(e.target.value)}
+                >
                     <option className='input-field-c'>Annually</option>
                     <option className='input-field-c'>Monthly</option>
                 </select>
             </label>
         </form>
+        </>
     );
 }
 
